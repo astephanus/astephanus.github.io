8d12f098f74cef109780538b66ec6b358a40be0f
Aaron Stephanus <taoist.futility@pm.me>
2023-11-02 13:49:46 -0600

    Updates site content with latest build
    Signed-off-by: Aaron Stephanus <taoist.futility@pm.me>

--- assets/js/version-selector.js
+++ assets/js/version-selector.js
@@ -1,9 +1,9 @@
 (() => {
 
-const DOC_VERSIONS = ["2.10","1.3"];
-const DOC_VERSIONS_ARCHIVED = ["2.9","2.8","2.7","2.6","2.5","2.4","2.3","2.2","2.1","2.0","1.2","1.1","1.0"];
+const DOC_VERSIONS = ["2.11","1.3"];
+const DOC_VERSIONS_ARCHIVED = ["2.10","2.9","2.8","2.7","2.6","2.5","2.4","2.3","2.2","2.1","2.0","1.2","1.1","1.0"];
 
-const DOC_VERSION_LATEST = "2.10";
+const DOC_VERSION_LATEST = "2.11";
 
 /* During build, DOC_VERSIONS is prefixed to convey all the versions available, informed by `_data/versions.json`
  * Example:
